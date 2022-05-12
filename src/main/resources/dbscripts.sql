898e34c7c837bf2d1b6432471c10562d2b8010a9
saitej-javadev <100029346+saitej-javadev@users.noreply.github.com>
2022-05-12 12:18:30 +0530

    Update dbscripts.sql

--- src/main/resources/dbscripts.sql
+++ src/main/resources/dbscripts.sql
@@ -1,3 +1,4 @@
+use mydb;
 create table bankaccount( id int primary key auto_increment,accno int ,firstname varchar(25), lastname varchar(20),  bal int) ;
 
 insert into bankaccount(accno,firstname,lastname,bal) values(12345,'Bill','Gates',5000);
